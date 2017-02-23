a6be3bf6ffe3870d88e3243d685da042f6ac9829
Patrick Rainsberry <prrainsberry@yahoo.com>
2017-02-23 12:59:50 -0800

    Added combine and rect pattern

--- Fusion360Utilities.py
+++ Fusion360Utilities.py
@@ -22,7 +22,7 @@ def get_app_objects():
     document = app.activeDocument
 
     # Get Design specific elements
-    units_manager = design.unitsManager
+    units_manager = design.fusionUnitsManager
     export_manager = design.exportManager
     root_comp = design.rootComponent
     time_line = product.timeline
@@ -119,3 +119,74 @@ def create_component(target_component, name):
     new_occurrence.component.name = name
 
     return new_occurrence
+
+
+# Creates rectangle pattern of bodies based on vectors
+def rect_body_pattern(target_component, bodies, x_axis, y_axis, x_qty, x_distance, y_qty, y_distance):
+    move_feats = target_component.features.moveFeatures
+
+    x_bodies = adsk.core.ObjectCollection.create()
+    all_bodies = adsk.core.ObjectCollection.create()
+
+    for body in bodies:
+        x_bodies.add(body)
+        all_bodies.add(body)
+
+    for i in range(1, x_qty):
+
+        # Create a collection of entities for move
+        x_source = adsk.core.ObjectCollection.create()
+
+        for body in bodies:
+            new_body = body.copyToComponent(target_component)
+            x_source.add(new_body)
+            x_bodies.add(new_body)
+            all_bodies.add(new_body)
+
+        x_transform = adsk.core.Matrix3D.create()
+        x_axis.normalize()
+        x_axis.scaleBy(x_distance * i)
+        x_transform.translation = x_axis
+
+        move_input_x = move_feats.createInput(x_source, x_transform)
+        move_feats.add(move_input_x)
+
+    for j in range(1, y_qty):
+        # Create a collection of entities for move
+        y_source = adsk.core.ObjectCollection.create()
+
+        for body in x_bodies:
+            new_body = body.copyToComponent(target_component)
+            y_source.add(new_body)
+            all_bodies.add(new_body)
+
+        y_transform = adsk.core.Matrix3D.create()
+        y_axis.normalize()
+        y_axis.scaleBy(y_distance * j)
+        y_transform.translation = y_axis
+
+        move_input_y = move_feats.createInput(y_source, y_transform)
+        move_feats.add(move_input_y)
+
+    return all_bodies
+
+
+# Creates Combine Feature in target with all tool bodies as source
+# Specify operation as: adsk.fusion.FeatureOperations
+# target_body -> single body
+# tool_bodies -> list of bodies
+def combine_feature(target_body, tool_bodies, operation):
+
+    # Get Combine Features
+    combine_features = target_body.parentComponent.features.combineFeatures
+
+    # Define a collection and add all tool bodies to it
+    combine_tools = adsk.core.ObjectCollection.create()
+    for tool in tool_bodies:
+        # todo add error checking
+        combine_tools.add(tool)
+
+    # Create Combine Feature
+    combine_input = combine_features.createInput(target_body, combine_tools)
+    combine_input.operation = operation
+    combine_features.add(combine_input)
